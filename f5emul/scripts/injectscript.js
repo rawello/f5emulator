8675afc32b5f6f9542ea7589695049f97af24f28
Ravenmis <49607391+Ravenmis@users.noreply.github.com>
2020-05-11 17:28:43 +0400

    Update injectscript.js

--- f5emul/scripts/injectscript.js
+++ f5emul/scripts/injectscript.js
@@ -1,40 +1,57 @@
 function initYAPF5simylator() {
-
-    const updtInterval = setInterval(goUpdate, 10000); //-- 30 ���
-    let postsCount = $("table[id^='p_row_']").length;
-    const pages = $('.row3:first tr:last td:first a[href!="#"]').length;
-
+    let updtInterval = setInterval(goUpdate, 5000);
+    let postsCount = ($("table[id^='p_row_']")).length;
+    let pages = $('.row3:first tr:last td:first a[href!="#"]').length;
     function goUpdate() {
-        window.postMessage({ type: "loadUpdatePost", url: window.location.href }, "*");
-    }
-
+        window.postMessage({ type: "loadUpdatePost", url: window.location.href }, "*");}
+    setInterval("updte()", 5000);
     window.addEventListener("message", function(event) {
-        if (event.source !== window) return;
-        if (event.data.type && (event.data.type === "showUpdatePost")) {
-            var _html = $(event.data.updtPost);
-            var updtPosts = _html.find("table[id^='p_row_']");
-            var updtPages = _html.find('.row3:first tr:last td:first a[href!="#"]');
-            if (updtPages.length > pages) { //-- ���� ����� ��������
-                if (confirm('���� ����� ��������, �������?')) {
-                    window.location.href = updtPages.eq(updtPages.length - 2).attr("href");
-                } else {
-                    clearInterval(updtInterval); //-- ������ �� ��������� � ���������
-                }
+        $('.f5new').hover(
+            function() {
+                $("#peep1").removeClass("f5simylatorpost f5new");
+                $("#peep2").removeClass("f5simylatorpost f5new");
+                $("#peep3").removeClass("f5simylatorpost f5new");
+                $("#peep4").removeClass("f5simylatorpost f5new");
+                $("#peep5").removeClass("f5simylatorpost f5new");
+                $("#peep6").removeClass("f5simylatorpost f5new");
+                $("#peep7").removeClass("f5simylatorpost f5new");
+                $("#peep8").removeClass("f5simylatorpost f5new");
+                $("#peep9").removeClass("f5simylatorpost f5new");
+                $("#peep10").removeClass("f5simylatorpost f5new");
+                $("#peep11").removeClass("f5simylatorpost f5new");
+                $("#peep12").removeClass("f5simylatorpost f5new");
+                $("#peep13").removeClass("f5simylatorpost f5new");
+                $("#peep14").removeClass("f5simylatorpost f5new");
+                $("#peep15").removeClass("f5simylatorpost f5new");
+                $("#peep16").removeClass("f5simylatorpost f5new");
+                $("#peep17").removeClass("f5simylatorpost f5new");
+                $("#peep18").removeClass("f5simylatorpost f5new");
+                $("#peep19").removeClass("f5simylatorpost f5new");
+                $("#peep20").removeClass("f5simylatorpost f5new");
+                $("#peep21").removeClass("f5simylatorpost f5new");
+                $("#peep22").removeClass("f5simylatorpost f5new");
+                $("#peep23").removeClass("f5simylatorpost f5new");
+                $("#peep24").removeClass("f5simylatorpost f5new");
+                $("#peep25").removeClass("f5simylatorpost f5new");
+                $("#peep26").removeClass("f5simylatorpost f5new");
             }
-            if (updtPosts.length <= postsCount) return;
-            for (var i = postsCount; i < updtPosts.length; i++) {
-                $('form[name="collect"]').append('<table width="100%" border="0" cellspacing="1" cellpadding="3" id="peep' + i + '" class="f5simylatorpost f5new">' + updtPosts.eq(i).html() + '</table>' + '<div class="darkrow1" style="height:5px"></div>');
+        )
+        if (event.source !== window) { return; }
+        if (event.data.type) {
+            if (event.data.type === "showUpdatePost") {
+                let _html = $(event.data.updtPost);
+                let updtPosts = _html.find("table[id^='p_row_']");
+                if (updtPosts == null) { alert('ahtung'); }
+                let updtPages =
+                    _html.find('.row3:first tr:last td:first a[href!="#"]');
+                if (updtPages.length > pages) {alert('Новая страница');}
+                if (updtPosts.length <= postsCount) { return; }
+                for (let i = postsCount; i < updtPosts.length; i++) {
+                    $('form[name="collect"]').append('<table width="100%" ' + 'border="0" cellspacing="1" cellpadding="3" id="peep' + i + '" class="f5simylatorpost f5new">' + updtPosts.eq(i).html() + '</table>' + '<div class="darkrow1" style="height:5px"></div>');}
+                postsCount += (updtPosts.length - postsCount);
+                $('link[rel="shortcut icon"]').attr("href", event.data.favicon);
+                if (postsCount => 1) {alert('Новый пост');}
             }
-            postsCount += (updtPosts.length - postsCount);
-            $('link[rel="shortcut icon"]').attr("href", event.data.favicon);
-
         }
     });
-
-    $(".f5new").livequery("mouseover", function() {
-        $(this).removeClass("f5new");
-        if ($(".f5new").length === 0) $('link[rel="shortcut icon"]').attr("href", "/favicon.ico");
-        return false;
-    });
-
 }
